5accfc6abc0adc457bd4c4da598381b948724240
muraasra <wilfriedtayou6@gmail.com>
2025-05-18 07:24:18 +0200

    Fix build configuration for Netlify deployment

--- Frontend/nuxt.config.ts
+++ Frontend/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
+  nitro: {
+    preset: 'node-server'
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@pinia/nuxt'],
   app: {
